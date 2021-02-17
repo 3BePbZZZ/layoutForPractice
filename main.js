3a6397f92602725b213d44a19c4ad1a6ea543639
Oslan <61590805+3BePbZZZ@users.noreply.github.com>
2021-02-17 16:21:33 +0300

    Update 0.3
    Added: check form and etc.

---
+++ main.js
@@ -0,0 +1,34 @@
+  'use strict';
+
+  (() => {
+    /* fields / inputs */
+    const nameField = document.querySelector('#nameField');
+    const phoneField = document.querySelector('#phoneField');
+
+    /* Regular expensions */
+    const phoneReg =   /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/;
+    const nameReg = /^[_.-a-zа-я]{3,16}$/i;
+
+    /* Check name and phone */
+    const callBtn = document.querySelector('#callBtn');
+    callBtn.addEventListener('click', () => {
+    	const isUncorrectName = isUncorrectValue(nameReg, nameField, 'uncorrect');
+    	const isUncorrectPhone = isUncorrectValue(phoneReg, phoneField, 'uncorrect');
+  
+      if(isUncorrectName && isUncorrectPhone) {
+        console.log('true');
+      }
+  
+    });
+  })();
+
+  /* Check correct value */
+  function isUncorrectValue(reg, input, className) {
+  	if( reg.test(input.value) ) {
+      input.classList.remove(className);
+      return true;
+    } 
+
+    input.classList.add(className);
+    return false;
+  }
